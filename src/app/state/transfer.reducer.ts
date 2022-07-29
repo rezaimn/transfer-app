f538d76a6fa785615a360ed0bbb88227a22280f0
Mohammadreza Imani <mohammadreza.i@agileful.com>
2022-07-29 14:40:09 +0430

    fix reducer

--- src/app/state/transfer.reducer.ts
+++ src/app/state/transfer.reducer.ts
@@ -1,4 +1,4 @@
-import { createReducer, on, Action } from '@ngrx/store';
+import {createReducer, on, Action} from '@ngrx/store';
 import {addTransfer, removeTransfer, retrievedTransferList, updateTransfer} from './transfer.actions';
 import {TransfersModel} from '../tab1/transfers/transfers.model';
 
@@ -7,8 +7,23 @@ export const initialState: ReadonlyArray<TransfersModel> = [];
 
 export const transfersReducer = createReducer(
   initialState,
-  on(retrievedTransferList, (state, { transfers }) => [...transfers]),
-  on(addTransfer, (state, { transfer }) => [...state,transfer]),
-  on(updateTransfer, (state, { transfer, transferId }) => state),
-  on(removeTransfer, (state, { transferId }) => [...state])
+  on(retrievedTransferList, (state, {transfers}) => [...transfers]),
+  on(addTransfer, (state, {transfer}) => [...state, transfer]),
+  on(updateTransfer, (state, {transfer, transferId}) => {
+    const array = [...state];
+    const index = array.findIndex(t => t.id === transferId);
+    if (index !== -1) {
+      array[index] = transfer;
+    }
+    console.log(index,transfer)
+    return array;
+  }),
+  on(removeTransfer, (state, {transferId}) => {
+    const array = [...state]; // make a separate copy of the array or state
+    const index = array.findIndex(transfer => transfer.id === transferId);
+    if (index !== -1) {
+      array.splice(index, 1);
+      return array;
+    }
+  })
 );
